4d19aac67896727bdb91445641e4582fb1416592
Nelson Diaz <abnerdc86@gmail.com>
2018-06-14 17:13:01 -0400

    Shuffle Method added to the cards.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Header from "./components/Header";
 import Footer from './components/Footer';
+import Wrapper from "./components/Wrapper";
 
 class App extends React.Component {
+  shuffleElements = elementsArray => {
+    var ctr = elementsArray.length, temp, index;
+    while (ctr > 0) {
+      // Pick a random index
+      index = Math.floor(Math.random() * ctr);
+      ctr--;
+      // And swap the last element with it
+      temp = elementsArray[ctr];
+      elementsArray[ctr] = elementsArray[index];
+      elementsArray[index] = temp;
+    }
+    return elementsArray;
+  }
+
   render() {
     return (
       <div className="App">
-        <Header/>
-        <Footer/>
+        <Header />
+        <Wrapper>
+
+        </Wrapper>
+        <Footer />
       </div>
     );
   }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Card.css";
 
-class FriendCard extends React.Component {
+class Card extends React.Component {
   state = {
     clicked: false
   };
 
   handleClick = (event) => {
-    this.props.removeFriend(this.props.id);
+
   }
   render() {
     return (
@@ -15,25 +15,10 @@ class FriendCard extends React.Component {
         <div className="img-container">
           <img alt={this.props.name} src={this.props.image} />
         </div>
-        <div className="content">
-          <ul>
-            <li>
-              <strong>Name:</strong> {this.props.name}
-            </li>
-            <li>
-              <strong>Occupation:</strong> {this.props.occupation}
-            </li>
-            <li>
-              <strong>Location:</strong> {this.props.location}
-            </li>
-          </ul>
-        </div>
-        <span onClick={this.removeFromBoard}
-          className="remove">𝘅</span>
       </div>
 
     );
   }
 }
 
-export default FriendCard;
+export default Card;
